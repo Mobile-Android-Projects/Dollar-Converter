f37659889f34380874dcb98e28ee7bc1c9ba6578
Auravision <auravision.africa@gmail.com>
2021-10-14 10:49:59 +0300

    Winged it and it worked.

--- app/src/main/java/com/example/primaldroid/MainActivity.kt
+++ app/src/main/java/com/example/primaldroid/MainActivity.kt
@@ -2,17 +2,40 @@ package com.example.primaldroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.primaldroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var dollarAmntEDTX:EditText
+    private lateinit var convertBtn:Button
+    private lateinit var displayTxt:TextView
+    private var euroRate: Float = 0.86f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //get instance of the binding class
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+        dollarAmntEDTX = binding.editText;
+        convertBtn = binding.convertBtn;
+        displayTxt = binding.textView;
         setContentView(view)
+    }
+
+    fun convertCurrency(view: View) {
+        //get the amount typed into the edit text
+        var amountTyped = dollarAmntEDTX.text.toString()
+        //cache in a local variable
+        //turn it to a number type
+        var dollarAmnt = amountTyped.toFloat()
+        //convert to dollars using a rate
+        dollarAmnt *= euroRate
+        displayTxt.text = dollarAmnt.toString()
+
     }
 }
